831091bb0629113e9231fbeffe97fa1e3f34abfa
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-20 11:21:17 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Expense Tracker</h1>
+    <ExpenseForm @add-expense="addExpense" />
+    <ExpenseList :expenses="expenses" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import ExpenseForm from './components/ExpenseForm.vue';
+import ExpenseList from './components/ExpenseList.vue';
+
+export default {
+  components: {
+    ExpenseForm,
+    ExpenseList,
+  },
+  setup() {
+    const expenses = ref([]);
+
+    const addExpense = (expense) => {
+      expenses.value.push(expense);
+    };
+
+    return {
+      expenses,
+      addExpense,
+    };
+  },
+};
+</script>
