71f469c6c78ab4335f03bcd82581a5d2d82c66ed
George Ibarra <gibarr13@stu.southtexascollege.edu>
2024-09-21 22:01:57 -0500

    Finished expense list component

---
+++ src/ExpnseList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>Expenses</h2>
+      <ul>
+        <ExpenseItem v-for="expense in expenses" :key="expense.name" :expense="expense" />
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import ExpenseItem from './ExpenseItem.vue';
+  
+  export default {
+    props: {
+      expenses: Array,
+    },
+    components: {
+      ExpenseItem,
+    },
+  };
+  </script>
+  
